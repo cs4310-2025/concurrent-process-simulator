606ff2eb2d1d01ce659154394bb2faf42250b55d
Inevitabby <161525738+Inevitabby@users.noreply.github.com>
2025-11-13 13:15:41 -0800

    add: Simple multicore test

--- .TMP.sh
+++ .TMP.sh
@@ -5,10 +5,14 @@ javac -d bin ../src/com/simulator/*/*.java
 
 # TESTS (simple one core)
 
-# FCFS
-java -cp bin com.simulator.injector.ProcessInjector --workload ../workloads/workload.txt | java -cp bin com.simulator.core.CPUSimulator --cores 1 --scheduler FCFS --ipc pipe
+# # FCFS
+# java -cp bin com.simulator.injector.ProcessInjector --workload ../workloads/workload.txt | java -cp bin com.simulator.core.CPUSimulator --cores 1 --scheduler FCFS --ipc pipe
+#
+# # SJF
+# java -cp bin com.simulator.injector.ProcessInjector --workload ../workloads/workload-sjf.txt | java -cp bin com.simulator.core.CPUSimulator --cores 1 --scheduler SJF --ipc pipe
+#
+# # PRIO
+# java -cp bin com.simulator.injector.ProcessInjector --workload ../workloads/workload-prio.txt | java -cp bin com.simulator.core.CPUSimulator --cores 1 --scheduler PRIORITY --ipc pipe
 
-# SJF
-java -cp bin com.simulator.injector.ProcessInjector --workload ../workloads/workload-sjf.txt | java -cp bin com.simulator.core.CPUSimulator --cores 1 --scheduler SJF --ipc pipe
-
-java -cp bin com.simulator.injector.ProcessInjector --workload ../workloads/workload-prio.txt | java -cp bin com.simulator.core.CPUSimulator --cores 1 --scheduler PRIORITY --ipc pipe
+# Multicore
+java -cp bin com.simulator.injector.ProcessInjector --workload ../workloads/workload-multicore.txt | java -cp bin com.simulator.core.CPUSimulator --cores 4 --scheduler PRIORITY --ipc pipe
