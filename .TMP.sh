147f8a1e8ad03a8c19032e9253d732e2f1ab48ad
Inevitabby <161525738+Inevitabby@users.noreply.github.com>
2025-11-13 13:07:26 -0800

    add: Rudimentary runner

---
+++ .TMP.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Yes, organization is poop right now.
+cd ./src || exit
+javac -d bin ../src/com/simulator/*/*.java
+
+# TESTS (simple one core)
+
+# FCFS
+java -cp bin com.simulator.injector.ProcessInjector --workload ../workloads/workload.txt | java -cp bin com.simulator.core.CPUSimulator --cores 1 --scheduler FCFS --ipc pipe
+
+# SJF
+java -cp bin com.simulator.injector.ProcessInjector --workload ../workloads/workload-sjf.txt | java -cp bin com.simulator.core.CPUSimulator --cores 1 --scheduler SJF --ipc pipe
+
+java -cp bin com.simulator.injector.ProcessInjector --workload ../workloads/workload-prio.txt | java -cp bin com.simulator.core.CPUSimulator --cores 1 --scheduler PRIORITY --ipc pipe
